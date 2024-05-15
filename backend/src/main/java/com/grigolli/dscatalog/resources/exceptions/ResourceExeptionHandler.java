87b8807e8ab23c4f0ca077f33bf8f769ab561673
Pedro Grigolli <pedrogrigolli30@gmail.com>
2024-05-15 16:36:26 -0300

    Delete Category

--- backend/src/main/java/com/grigolli/dscatalog/resources/exceptions/ResourceExeptionHandler.java
+++ backend/src/main/java/com/grigolli/dscatalog/resources/exceptions/ResourceExeptionHandler.java
@@ -7,15 +7,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.grigolli.dscatalog.services.exceptions.DatabaseException;
 import com.grigolli.dscatalog.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
-public class ResourceExecptionHandler {
+public class ResourceExeptionHandler {
     
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request){
+        // HttpStatus status = HttpStatus.NOT_FOUND;
+
         StandardError err = new StandardError();
         err.setTimestamp(Instant.now());
         err.setStatus(HttpStatus.NOT_FOUND.value());
@@ -25,4 +28,22 @@ public class ResourceExecptionHandler {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
 
     }
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request){
+        // HttpStatus status = HttpStatus.BAD_REQUEST;
+
+        StandardError err = new StandardError();
+        err.setTimestamp(Instant.now());
+        err.setStatus(HttpStatus.BAD_REQUEST.value());
+        err.setError("Database exception");
+        err.setMessage(e.getMessage());
+        err.setPath(request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+
+    }
+
+
+
+
+
 }
--- backend/src/main/java/com/grigolli/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/grigolli/dscatalog/services/CategoryService.java
@@ -5,12 +5,15 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.grigolli.dscatalog.dto.CategoryDTO;
 import com.grigolli.dscatalog.entities.Category;
 import com.grigolli.dscatalog.repositories.CategoryRepository;
+import com.grigolli.dscatalog.services.exceptions.DatabaseException;
 import com.grigolli.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -69,6 +72,21 @@ public class CategoryService {
             throw new ResourceNotFoundException("Id not found" + id);
         }
 
+
+
+    }
+
+
+    public void delete(Long id) {
+        try{
+            repository.deleteById(id);
+        }
+        catch (EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException("Id not found" + id);
+        }
+        catch (DataIntegrityViolationException e){
+            throw new DatabaseException("Integrity violation");
+        }
 
 
     }
